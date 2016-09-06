044b9678a7e3fad0d821d56fde428210d8d9a382
jspeaker <james@iontech.org>
2016-09-06 13:37:13 -0700

    Updated nuspec with license; included license in project; added license txt to src files

--- src/JsTest.Utilities.Extensions.js
+++ src/JsTest.Utilities.Extensions.js
@@ -1,8 +1,33 @@
 ﻿/* ******************************
- * Author: James Speaker
- * Copyright 2014-2016, iOnTech
- * james@iontech.org
+Author: James Speaker
+Copyright 2014-2016, iOnTech
+james@iontech.org
+
+Copyright (c) 2016 Jim Speaker
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
  * *************************** */
+
 var JsTests = JsTests || {
   testFrame: null
 };
@@ -10,9 +35,11 @@ var JsTests = JsTests || {
 JsTests.Configuration = {};
 
 JsTests.Spies = function () {
+  // ReSharper disable CallerCalleeUsing
   if (!(this instanceof arguments.callee)) {
     return new arguments.callee();
   }
+  // ReSharper restore CallerCalleeUsing
 
   var ns = JsTests.Fixture.namespace();
 
@@ -25,9 +52,11 @@ JsTests.Spies = function () {
 };
 
 JsTests.Account = function () {
+  // ReSharper disable CallerCalleeUsing
   if (!(this instanceof arguments.callee)) {
     return new arguments.callee();
   }
+  // ReSharper restore CallerCalleeUsing
 
   var ns = JsTests.Fixture.namespace();
 
